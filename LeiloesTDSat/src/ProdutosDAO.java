af2e66511564b58a4ca7bafafbf9967c5a54a2f2
CalorinaLima <carolinalimafontoura4@gmail.com>
2024-03-11 23:29:39 -0300

    Criada a tela de vendas

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -24,7 +24,7 @@ public class ProdutosDAO {
     }
 
     void venderProduto(int parseInt) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
 
 }
